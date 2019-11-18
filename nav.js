4d423c2345a11ed61769360b17aec6b3ee522e63
Jayda Medina <43220330+jmedina7@users.noreply.github.com>
2019-11-18 02:33:45 -0500

    nav styling

--- nav.js
+++
@@ -1,9 +0,0 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("mySidenav");
-  if (x.className === "sidenav") {
-    x.className += " responsive";
-  } else {
-    x.className = "sidenav";
-  }
-}
